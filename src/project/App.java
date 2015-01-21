6e22486f58ba9b7a75a56f20454cf5ccb5b4c65e
Roberto Zen <rob.zenn92@hotmail.it>
2015-01-21 15:58:05 +0100

    hostname and port changed for heroku

--- src/project/App.java
+++ src/project/App.java
@@ -11,17 +11,20 @@ import org.glassfish.jersey.server.ResourceConfig;
 public class App
 {
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
-    {    	
-    	String protocol = Settings.BASE_PROTOCOL;
-        String port = ":" + Settings.BASE_PORT;
-        String hostname = Settings.BASE_URL;
+    {
+        String protocol = Settings.BASE_PROTOCOL;
+        String port = System.getenv("PORT");
+        if (port == null || port.isEmpty()) {
+            port = Settings.BASE_PORT;
+        }
+        String hostname = InetAddress.getLocalHost().getHostAddress();
         String path = Settings.SERVICE_PATH;
         if (hostname.equals(Settings.BASE_URL))
         {
             hostname = "localhost";
         }
 
-        URI baseUrl = new URI(protocol + hostname + port + path);
+        URI baseUrl = new URI(protocol + hostname + ":" + port + path);
         System.out.println("Starting sdelab standalone HTTP server..");
         JdkHttpServerFactory.createHttpServer(baseUrl, createApp());
         System.out.println("server starts on " + baseUrl + "\n [kill the process to exit]");
