cc9fb4f60fb4d89f3b27142f33aca18cfc05391c
Roberto Zen <rob.zenn92@hotmail.it>
2015-02-10 21:28:53 +0100

    Settings updated

--- src/project/App.java
+++ src/project/App.java
@@ -13,16 +13,9 @@ public class App
     public static void main(String[] args) throws IllegalArgumentException, IOException, URISyntaxException
     {
         String protocol = Settings.BASE_PROTOCOL;
-        String port = System.getenv("PORT");
-        if (port == null || port.isEmpty()) {
-            port = Settings.BASE_PORT;
-        }
-        String hostname = InetAddress.getLocalHost().getHostAddress();
+        String port = Settings.BASE_PORT;
+        String hostname = Settings.BASE_URL;
         String path = Settings.SERVICE_PATH;
-        if (hostname.equals(Settings.BASE_URL))
-        {
-            hostname = "localhost";
-        }
 
         URI baseUrl = new URI(protocol + hostname + ":" + port + path);
         System.out.println("Starting standalone HTTP server..");
--- src/project/Settings.java
+++ src/project/Settings.java
@@ -4,7 +4,7 @@ public final class Settings {
 
 	//	Server Settings
 	public static final String BASE_PROTOCOL = "http://";
-	public static final String BASE_URL = "localhost";
+	public static final String BASE_URL = "188.226.183.46";
 	public static final String BASE_PORT = "8020";
 	public static final String SERVICE_PATH = "/project-facebook/";
 	
